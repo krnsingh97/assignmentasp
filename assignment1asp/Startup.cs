48b477c09c14f4e7b9fb480adb76dc38b4729933
karan singh <singhatwalkaran@gmail.com>
2018-10-12 04:40:01 -0400

    assignment1

---
+++ assignment1asp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(assignment1asp.Startup))]
+namespace assignment1asp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
